3c7d7115f8a583cf04ecba4e7323796a851d0178
Daniel Bonnell <daniel.bonnell@gmail.com>
2015-08-30 12:25:28 -0400

    Add new delegation methods

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,5 +1,27 @@
 class Exercise < ActiveRecord::Base
     belongs_to :day
     belongs_to :user
-    has_many :sets
+    has_many :set_logs
+
+    def dates
+        set_logs.pluck(:created_at).map {|x| "#{x.month}/#{x.day}"}
+    end
+
+    def cumulative_max_weight_days
+        # returns an hash like this:
+        # => {"8/29"=>180, "8/30"=>400}
+        hash = {}
+        set_logs.order(created_at: :asc).pluck(:created_at).each do |x|
+            hash["#{x.month}/#{x.day}"] = set_logs.date("#{x.year}-#{x.month}-#{x.day}").pluck(:weight, :reps).map {|x| x[0] * x[1]}.reduce(:+)
+        end
+    end
+
+    def one_rep_max_weight_days
+        # returns an hash like this:
+        # => {"8/29"=>15, "8/30"=>25}
+        hash = {}
+        set_logs.order(created_at: :asc).pluck(:created_at).each do |x|
+            hash["#{x.month}/#{x.day}"] = set_logs.date("#{x.year}-#{x.month}-#{x.day}").pluck(:weight).max
+        end
+    end
 end
